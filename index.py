4b8040a61682bd0547ca561ac4dd441e1d4a9d77
tiga_alves <thiago.damascena.alves@gmail.com>
2023-07-30 11:42:16 -0300

    criei o index

---
+++ index.py
@@ -0,0 +1,10 @@
+from dash import html, dcc, Input, Output
+import plotly.express as px
+import plotly.graph_objects as go
+import pandas as pd
+
+from app import *
+from dash_bootstrap_templates import ThemeSwitchAIO
+
+
+
