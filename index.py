643c3b233b2928878a367f2c00352a3f80cc8c4c
tiga_alves <thiago.damascena.alves@gmail.com>
2023-07-31 17:04:24 -0300

    clean up dos dados e leitura

--- index.py
+++ index.py
@@ -11,3 +11,7 @@ url_theme1 = dbc.themes.VAPOR
 url_theme2 = dbc.themes.FLATLY
 template_theme1 = 'vapor'
 template_theme2 = 'flatly'
+
+# Reading data ==========================
+df = pd.read_csv('data_clean.csv')
+state_options = [{'label': x, 'value': x} for x in df['ESTADO'].unique()]
