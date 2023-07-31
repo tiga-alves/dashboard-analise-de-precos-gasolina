5c4e5bca11cb75f82f346c34417da928dce90f13
tiga_alves <thiago.damascena.alves@gmail.com>
2023-07-31 17:05:51 -0300

    adicionando o switch de tema e grafico atemporal

--- index.py
+++ index.py
@@ -15,3 +15,29 @@ template_theme2 = 'flatly'
 # Reading data ==========================
 df = pd.read_csv('data_clean.csv')
 state_options = [{'label': x, 'value': x} for x in df['ESTADO'].unique()]
+
+# Layout ================================
+app.layout = dbc.Container([
+    # Row 1 
+    dbc.Row([
+        dbc.Col([
+            ThemeSwitchAIO(aio_id='theme', themes=[url_theme1, url_theme2]),
+            html.H1('Preco x Estado'),
+            html.H3('Comparação temporal entre estados'),
+            dcc.Dropdown(
+                id='estados',
+                value=[state['label'] for state in state_options[:3]],
+                multi=True,
+                options=state_options
+            )
+        ]),
+        dcc.Graph(id='line_graph')       
+    ]),
+    
+    # Row 2 
+    dbc.Row([
+        dbc.Col([
+            
+        ])
+    ])
+])
